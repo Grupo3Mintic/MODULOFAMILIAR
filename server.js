b33346508e9069b7910830ade441b3a87163356a
Julio <julio889@hotmail.com>
2025-07-03 13:26:07 -0500

    🔧 Agregar CORS para permitir conexión desde app Cordova

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 // server.js
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-require("dotenv").config();
 
 // Conexión a base de datos
 const db = require("./server/database");
 
 // Middlewares
+app.use(cors()); // Habilita CORS
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "www")));
 
@@ -21,5 +24,5 @@ app.use("/api/grupo", grupoRoutes);
 
 // Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`✅ Servidor activo en http://localhost:${PORT}`);
+  console.log(`✅ Servidor activo en el puerto ${PORT}`);
 });
